2d7ebd3b502b892d08bed99545d0f9d6145b1ecd
koka0630 <youmokakan.220.284@gmail.com>
2022-03-27 06:24:00 +0900

    sending mail

--- passwordup2.php
+++ passwordup2.php
@@ -1,5 +1,15 @@
 <?php
 require_once 'dbconnect.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+// 設置した場所のパスを指定する
+require('PHPMailer/src/PHPMailer.php');
+require('PHPMailer/src/Exception.php');
+require('PHPMailer/src/SMTP.php');
+
 $passwordup = $_POST['mail'];
 $errmessage =  array();
 
@@ -19,8 +29,85 @@ $stmt->execute();
 $result = $stmt->fetchAll();
 
 if(!$result){
-    echo "投稿がありません";
+    echo "存在しないユーザです。";
   }
+
+// 以下はこの記事を参考にした
+// https://qiita.com/ShibuyaKosuke/items/309c0a7d969baf0ea8d1
+
+// コンストラクタ
+$mail = new PHPMailer();
+
+// 文字コード
+$mail->CharSet = "iso-2022-jp";
+$mail->Encoding = "7bit";
+
+// SMTPサーバーを利用する
+$mail->IsSMTP();
+
+// デバッグ
+$mail->SMTPDebug = 1;
+
+// SMTPAuthを利用する
+$mail->SMTPAuth = true;
+
+// SMTPサーバー
+$mail->Host = 'smtp.gmail.com';
+
+// ユーザー名
+$sender_email = 'koyamatest42@gmail.com';//getenv('SENDER_EMAIL');
+$mail->Username = 'koyamatest42@gmail.com';//'koyamatest42@gmail.com';
+
+// パスワード
+$password = 'thozgcfhsdtihsni';//getenv('SENDER_PASSWORD');
+$mail->Password = 'thozgcfhsdtihsni';//'thozgcfhsdtihsni';
+
+// ポート
+$mail->Port = 587;
+
+// メールヘッダー文字コード
+$mimeheader_encoding = 'JIS';
+
+// 送信者アドレス
+$mail->From = $sender_email;
+
+// 送信者名
+$mail->FromName = mb_encode_mimeheader(
+    'ramen-man'
+    , $mimeheader_encoding
+);
+
+// 宛先
+$receiver_email = $passwordup;
+$mail->addAddress($receiver_email);
+
+// メールタイトル
+$mail->Subject = mb_encode_mimeheader(
+    '[ramen site] パスワード再設定用メール'
+    , $mimeheader_encoding
+);
+
+// メール本文
+$tmp_key=md5(uniqid(rand(),true));
+$SERVER_URL="localhost:8888";
+$reset_password_url = "https://$SERVER_URL/reset_password_form.php?tmp_key=$tmp_key";
+$mail->Body = mb_convert_encoding(
+    "パスワードの再設定用url: $reset_password_url"
+    , $mimeheader_encoding
+);
+
+$mail->SMTPSecure = 'tls';
+
+if (!$mail->send()) {
+    echo $mail->ErrorInfo;
+}
+$stmt =  $dbh->prepare("UPDATE users SET tmp_key = :tmp_key WHERE mail = :mail");
+$stmt->bindValue(':tmp_key', $tmp_key, PDO::PARAM_STR);
+$stmt->bindValue(':mail', $passwordup, PDO::PARAM_STR);
+
+$stmt->execute();
+$result = $stmt->fetchAll();
+
 
 ?>
 <!DOCTYPE html>
